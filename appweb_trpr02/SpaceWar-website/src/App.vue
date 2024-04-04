c7acc49f8ee1ef323d8a683b667864ce0478eb4b
Hubert Beaupre <2143562@csfoy.ca>
2024-04-04 17:05:40 -0400

    Trying to display App.vue

--- appweb_trpr02/SpaceWar-website/src/App.vue
+++ appweb_trpr02/SpaceWar-website/src/App.vue
@@ -4,5 +4,13 @@
 </script>
 
 <template>
-    
+    <header class="container-fluid bg-dark text-light p-4 text-center">
+      <h1>Bee Swarm Showdown</h1>
+    </header>
+    <main>
+        <p>test</p>
+    </main>
+    <footer class="container-fluid bg-secondary text-light p-1">
+      <p class="text-center">Copyright Hubert Beaupré, Jean-Thomas Lamonde, Onett</p>
+    </footer>
 </template>
