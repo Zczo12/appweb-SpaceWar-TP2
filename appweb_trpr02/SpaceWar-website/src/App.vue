e616a1afd760413a4ac09f95229a9e0a1360e57a
Hubert Beaupre <2143562@csfoy.ca>
2024-04-09 14:11:16 -0400

    Formulaire avec communication props

--- appweb_trpr02/SpaceWar-website/src/App.vue
+++ appweb_trpr02/SpaceWar-website/src/App.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 //import { RouterLink, RouterView } from 'vue-router'
-//import Acceuil from './component/Acceuil.vue';
+import Acceuil from './component/Acceuil.vue';
+
+/*
+const characters = ref<Characters[]>([]);
+const ships = ref<Ship[]>([]);
+const rankings = ref<Rank[]>([]);
+
+async function getCharacters() {
+  characters.value = await enemyService.getCharacters();
+}
+async function getShips() {
+  ships.value = await enemyService.getShips();
+}
+async function getRankings() {
+  characters.value = await enemyService.getRankings();
+}
+*/
 const playerName = ref<string | null>('');
 const shipName = ref<string | null>('');
 
+function handleUpdate(newPlayerName: string, newShipName: string){
+  playerName.value = newPlayerName;
+  shipName.value = newShipName;
+}
 
 </script>
 
@@ -13,13 +33,7 @@ const shipName = ref<string | null>('');
       <h1>Bee Swarm Showdown</h1>
     </header>
     <main>
-      <div>
-        <form>
-            <p>Nom du joueur: <input type="text" required v-model="playerName"</p>
-            <p>Nom du vaisseau: <input type="text" required v-model="shipName"</p>
-            <button type="submit">Lancer mission</button>
-        </form>
-    </div>
+      <Acceuil />
     </main>
     <footer class="container-fluid bg-secondary text-light p-1">
       <p class="text-center">Copyright Hubert Beaupré, Jean-Thomas Lamonde, Onett</p>
--- appweb_trpr02/SpaceWar-website/src/component/Acceuil.vue
+++ appweb_trpr02/SpaceWar-website/src/component/Acceuil.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
-//import { ref } from 'vue'
+const emits = defineEmits(['update']);
 
+function notifyParent(newPlayerName: string, newShipName: string): void{
+  emits('update', newPlayerName, newShipName);
+}
+
+let currentPlayerName: string = '';
+let currentShipName: string = '';
+
+function setPlayerInfos(newPlayerName: string, newShipName: string){
+    currentPlayerName = newPlayerName;
+    currentShipName = newShipName;
+    notifyParent(newPlayerName, newShipName);
+}
+
+const handleSubmit = (event: Event) => {
+    //TODO verifications
+    setPlayerInfos(currentPlayerName, currentShipName);
+}
 </script>
 
 <template>
-
+    <div>
+        <form @submit="handleSubmit">
+            <p>Nom du joueur: <input type="text" required v-model="currentPlayerName"</p>
+            <p>Nom du vaisseau: <input type="text" required v-model="currentShipName"</p>
+            <button type="submit">Lancer mission</button>
+        </form>        
+    </div>
 </template>
